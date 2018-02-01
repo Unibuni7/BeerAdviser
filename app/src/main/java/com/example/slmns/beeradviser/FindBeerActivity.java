58f058f18340247f31d30a11ff9cf62bb007c031
Unibuni7 <abdu0445@edu.easj.dk>
2018-02-01 16:35:21 +0100

    Beer Adviser
    My first Exercise

---
+++ app/src/main/java/com/example/slmns/beeradviser/FindBeerActivity.java
@@ -0,0 +1,19 @@
+package com.example.slmns.beeradviser;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FindBeerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    public void OnClickFindBeer(View view) {
+
+    }
+}
