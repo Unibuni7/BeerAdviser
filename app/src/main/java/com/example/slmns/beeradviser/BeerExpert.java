7dabfbe2e230d2812bb27544ce0b2dac9281a420
Unibuni7 <abdu0445@edu.easj.dk>
2018-02-03 21:03:42 +0100

    Done!!!

---
+++ app/src/main/java/com/example/slmns/beeradviser/BeerExpert.java
@@ -0,0 +1,24 @@
+package com.example.slmns.beeradviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by slmns on 03-02-2018.
+ */
+
+public class BeerExpert {
+
+    List<String> getBrands (String color){
+        List<String> brands = new ArrayList<String>();
+        if (color.equals("Amber")){
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+
+        return brands;
+    }
+}
--- app/src/main/java/com/example/slmns/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/example/slmns/beeradviser/FindBeerActivity.java
@@ -3,17 +3,54 @@ package com.example.slmns.beeradviser;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class FindBeerActivity extends Activity {
-
+    BeerExpert beerExpert = new BeerExpert();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+
     }
 
+
+    // Call when user clicks the button
     public void OnClickFindBeer(View view) {
+
+        // Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        // Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        // Get the selected item in the spinner
+        String BeerType = String.valueOf(color.getSelectedItem());
+
+        // Display the selected item
+       // brands.setText(BeerType); DELETE IF YOU WANT
+
+        // Get recommendations from BeerExpert class
+        List<String> brandList = beerExpert.getBrands(BeerType);
+
+        StringBuilder brandsFormatted = new StringBuilder();
+
+        for (String brand : brandList) {
+            brandsFormatted.append(brand).append('\n');
+            // Build a string displaying each brand on a new line.
+            // BuildString allow you to save and display more than one string.
+        }
+
+        // Display the Beers
+
+        brands.setText(brandsFormatted);
+
+        Log.d("MINE", "Clicked on FindBeer Button");
 
     }
 }
